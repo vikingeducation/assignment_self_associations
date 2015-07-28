e06b581a93df70ac927b22e95e23979f1d7127c6
Adam <adam.kinson@gmail.com>
2015-07-28 18:44:13 -0400

    build self-association

--- movies/app/models/movie_recommendation.rb
+++ movies/app/models/movie_recommendation.rb
@@ -1,8 +1,11 @@
 class MovieRecommendation < ActiveRecord::Base
 
-  belongs_to :base_movie,         :foreign_key => :movie_id
-  belongs_to :recommended_movie,  :foreign_key => :recommended_movie_id
+  belongs_to :base_movie,         :foreign_key => :movie_id,
+                                  :class_name => "Movie"
 
-  validates :movie_id, :uniqueness => { :scope => :recommended_movie_id }
+  belongs_to :recommended_movie,  :foreign_key => :recommended_movie_id,
+                                  :class_name => "Movie"
+
+  validates :recommended_movie_id, :uniqueness => { :scope => :movie_id }
 
 end
