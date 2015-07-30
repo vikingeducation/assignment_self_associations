c3365e62396657bd5dfa89e47d0797b858cf1594
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-30 14:28:42 -0400

    created seeding

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -1,9 +1,9 @@
 class Competitor < ActiveRecord::Base
   has_many :victories, foreign_key: :winner_id,
                        class_name: "Match"
-  has_many :beaten_competitors, through: :victories, source: :loser_id
+  has_many :beaten_competitors, through: :victories, source: :loser
 
   has_many :losses, foreign_key: :loser_id,
                        class_name: "Match"
-  has_many :competitors_beaten_by, through: :losses, source: :winner_id
+  has_many :competitors_beaten_by, through: :losses, source: :winner
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,22 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   Mayor.create(name: 'Emanuel', city: cities.first
+
+ENTRANTS = 16
+
+# Competitors: name, seeding
+ENTRANTS.times do |i|
+  Competitor.create(:name => "Competitor_#{i+1}", :seeding => i+1)
+end
+
+# Matches: winner_id, loser_id
+
+index = 1
+until index == ENTRANTS
+  (index+1).upto(ENTRANTS) do |i|
+    competitors = [index, i].shuffle
+    Match.create(:winner_id => competitors.pop, :loser_id => competitors.pop )
+  end
+  index += 1
+end
