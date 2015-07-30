730cd43d322121fd78d4c1f515f6669256e5f276
David <davidmeza@outlook.com>
2015-07-30 17:41:59 -0400

    testing stuff

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,11 +4,13 @@ class Person < ActiveRecord::Base
   has_many :treatments_received,  foreign_key: :patient_id,
                                   class_name: "Treating"
   has_many :treating_physicians,  through: :treatments_received,
-                                  source: :physicians
+                                  source: :physicians,
+                                  dependent: :destroy
 
   # A physician
   has_many :treatments_given,     foreign_key: :physician_id,
                                   class_name: "Treating"
   has_many :patients_treated,     through: :treatments_given,
-                                  source: :patients
+                                  source: :patients,
+                                  dependent: :destroy
 end
