5e6c7c7fb1b0e0b9f742637c9ba507669d1fb7e6
alokp <alokpra@gmail.com>
2015-07-30 13:02:49 -0700

    join relationships working

--- quick_app/app/models/ally.rb
+++ quick_app/app/models/ally.rb
@@ -1,2 +1,9 @@
 class Ally < ActiveRecord::Base
+	
+	belongs_to :ally_initiator, :foreign_key => :ally_intiator_id, :class_name => "Country"
+
+	belongs_to :ally_acceptor, :foreign_key => :ally_acceptor_id, :class_name => "Country"
+
+	validates :ally_acceptor_id, :uniqueness => {:scope => :ally_intiator_id}
+
 end
--- quick_app/db/seeds.rb
+++ quick_app/db/seeds.rb
@@ -7,10 +7,23 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Country.destroy_all
+Ally.destroy_all
 
 50.times do 
-  Country.create(name: Faker::Address.country, population:  rand(1000..20000))
+  Country.create(name: Faker::Address.country, population: rand(1000..20000))
+end
+
+
+count_of_countries = Country.count
+60.times do
+	country1_id = Country.offset(rand(count_of_countries)).first.id
+	country2_id = Country.offset(rand(count_of_countries)).first.id
+	Ally.create(ally_intiator_id: country1_id, ally_acceptor_id: country2_id) if country1_id != country2_id
 end
 
 
 
+
+	# until country1_id != country2_id
+	# 	country2_id = Country.offset(rand(count_of_countries)).first.id 
+	# end
