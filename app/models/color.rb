5df4c1b09af0e8e8b0fbd6d7cd9776fc9781697c
Sia <siakaramalegos@gmail.com>
2016-01-07 14:52:36 -0600

    sets up associations and seeds

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,2 +1,7 @@
 class Color < ActiveRecord::Base
+  has_many :color_pairs_as_base, foreign_key: :base_id, class_name: 'ColorPair'
+  has_many :accents, through: :color_pairs_as_base, source: :accent_color
+
+  has_many :color_pairs_as_accent, foreign_key: :accent_id, class_name: 'ColorPair'
+  has_many :bases, through: :color_pairs_as_accent, source: :base_color
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Color.destroy_all
+ColorPair.destroy_all
+
+colors = Color.create([{name: 'flamingo pink'}, {name: 'charcoal'}, {name: 'bright blue'}, {name: 'red hot red'}, {name: 'charcoal'}, {name: 'white'}, {name: 'black'}, {name: 'flaming orange'}, {name: 'racer yellow'}, {name: 'taupe'}, {name: 'beige'}, {name: 'vivid green'}])
+
+60.times do
+  b_id = Color.pluck(:id).sample
+  a_id = Color.pluck(:id).sample
+  if a_id == b_id || ColorPair.where("base_id = ? AND accent_id = ?", b_id, a_id).present?
+    next
+  end
+  ColorPair.create(base_id: b_id, accent_id: a_id)
+end
