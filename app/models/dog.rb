58445a2af5f0a1022dcd753933aac2a79bb5361f
Joshua Masland-Sarani <j.masland@gmail.com>
2016-02-09 18:23:46 -0500

    add reciprocated playings sql

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -13,4 +13,18 @@ class Dog < ActiveRecord::Base
                                 dependent: :destroy
   has_many :players,  through: :received_playings,
                       source: :play_initiator
+
+  def reciprocated_playings
+    sql = "
+      SELECT DISTINCT dogs.*
+      FROM dogs
+      JOIN playings
+        ON dogs.id = playings.player_id
+      JOIN playings AS reflected_playings
+        ON reflected_playings.player_id = playings.playee_id
+      WHERE reflected_playings.player_id = ?
+      "
+
+    Dog.find_by_sql([sql,self.id])
+  end
 end
