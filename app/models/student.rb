29ffb26bcaab12655d90603909a081c40d0b825e
Julia Herron <julia.herron@gmail.com>
2016-02-09 10:36:18 -0800

    testing out dependent: :nullify

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,13 +3,13 @@ class Student < ActiveRecord::Base
   # initiator
   has_many :initiated_enrollings, foreign_key: :enroller_id, class_name: "Enrolling"
 
-  has_many :professors, through: :initiated_enrollings, source: :enroller_recipient
+  has_many :professors, through: :initiated_enrollings, source: :enroller_recipient, dependent: :nullify
 
 
   # receiver
   has_many :received_enrollings, foreign_key: :professor_id, class_name: "Enrolling"
 
-  has_many :enrolled_students, through: :received_enrollings, source: :enroller_initiator, dependent: :destroy
+  has_many :enrolled_students, through: :received_enrollings, source: :enroller_initiator, dependent: :nullify
 
 
 end
