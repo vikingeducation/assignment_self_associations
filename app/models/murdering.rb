5764d3ac21428b00a69c851ba9ff01962cfe9d0b
Kit Langton <kitlangton@gmail.com>
2016-02-09 12:41:27 -0500

    murderings complete

--- app/models/murdering.rb
+++ app/models/murdering.rb
@@ -1,5 +1,4 @@
 class Murdering < ActiveRecord::Base
-
   belongs_to :murderer, class_name: "Person"
   belongs_to :victim, class_name: "Person"
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,21 +1,19 @@
 class Person < ActiveRecord::Base
-
-
   has_many :committed_murders,
            class_name: "Murdering",
-           foreign_key: :murderer_id
-
-  has_many :received_murders,
-           class_name: "Murdering",
-           foreign_key: :victim_id
+           foreign_key: :murderer_id,
+           :dependent => :destroy
 
   has_many :victims,
            class_name: "Person",
-           :through => :committed_murders,
+           :through => :committed_murders
+
+  has_many :received_murders,
+           class_name: "Murdering",
+           foreign_key: :victim_id,
            :dependent => :destroy
 
   has_many :murderers,
            class_name: "Person",
            :through => :received_murders
-
 end
