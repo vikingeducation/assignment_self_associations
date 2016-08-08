1996e4a3b87aaf5efaefd2f8928f0a60f29a7ee5
David Jiang <davidjgrey@gmail>
2016-08-08 11:59:25 -0700

    checked dependent destroy results

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,26 @@
 Station.destroy_all
 DirectConnection.destroy_all
 
+def choose_another_station(station)
+   station_to_add = Station.all.sample 
+  until station_to_add.id != station.id
+    station_to_add = Station.all.sample 
+  end
+  station_to_add
+end
 
 SEEDS = 20
 
 
 SEEDS.times do 
-    station_a = Station.new()
+    Station.create(:name => "#{Faker::Address.city} Station")
 end
+
+Station.all.each do |station|
+
+  station.origin_stations << choose_another_station(station)
+
+  station.destinations << choose_another_station(station)
+end
+
+
