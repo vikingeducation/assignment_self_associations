ac716a2d0c4b18e67698d963f568673764bd6855
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-08 10:57:13 -0700

    added models

--- app/models/neighboring.rb
+++ app/models/neighboring.rb
@@ -1,2 +1,5 @@
 class Neighboring < ActiveRecord::Base
+  belongs_to :neighbor_initator, foreign_key: :neighbor_id, class_name: "House"
+  belongs_to :neighbor, foreign_key: :neighboree, class_name: "House"
+  
 end
--- db/migrate/20160808172644_create_houses.rb
+++ db/migrate/20160808172644_create_houses.rb
@@ -1,6 +1,8 @@
 class CreateHouses < ActiveRecord::Migration
   def change
     create_table :houses do |t|
+      t.string :name
+      t.integer :house_number
 
       t.timestamps null: false
     end
