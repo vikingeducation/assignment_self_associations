7e9252be0bb13196a070d58ad8565ca5fad623ce
cjvirtucio87 <cjv287@gmail.com>
2016-08-08 10:52:06 -0700

    Finished.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,9 +1,9 @@
 class Animal < ActiveRecord::Base
 
   has_many :prey_eatings, :foreign_key => :predator_id, :class_name => "Eating"
-  has_many :preys, :through => :prey_eatings, :source => :prey, dependent: :destroy
+  has_many :preys, :through => :prey_eatings, :source => :prey
 
   has_many :predator_eatings, :foreign_key => :prey_id, :class_name => "Eating"
-  has_many :predators, :through => :predator_eatings, :source => :predator, dependent: :destroy
+  has_many :predators, :through => :predator_eatings, :source => :predator
 
 end
