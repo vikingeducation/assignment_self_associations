b573e781a885c31ea66a05f6963487f1b5bbc7d4
essian <jessicampdennis@hotmail.com>
2016-08-08 19:40:10 +0100

    adds seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+multiplier = 10
+
+Color.destroy_all
+Mixing.destroy_all
+
+multiplier.times do |n|
+  Color.create(name: Faker::Color.color_name, wavelength: (390..700).to_a.sample)
+end
+
+puts "#{multiplier} colors created"
+
+(multiplier/2).times do |n|
+
+  secondary_color = Color.all.sample.id
+  Mixing.create(primary_color_id: (Color.all.sample.id), secondary_color_id: secondary_color)
+  Mixing.create(primary_color_id: (Color.all.sample.id), secondary_color_id: secondary_color)
+
+end
+
+puts "Palette successfully mixed"
