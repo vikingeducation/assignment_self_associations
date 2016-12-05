3f867a7d1bb0b54d7bf61d7b823a0f0fb80b923a
John Sawyer <sawyer@sawyermerchant.com>
2016-12-05 15:05:46 -0500

    Optional True

--- app/models/colleague_connection.rb
+++ app/models/colleague_connection.rb
@@ -1,10 +1,11 @@
 class ColleagueConnection < ApplicationRecord
 
   belongs_to :inviting_colleague, foreign_key: :invitor_id,
-                                    class_name:  "Professional"
+                                    class_name:  "Professional",
+                                    optional: true
 
   belongs_to :receiving_colleague,   foreign_key: :invitee_id,
-                                    class_name:  "Professional"
+                                    class_name:  "Professional",
+                                    optional: true
 
-  validates :invitor_id, uniqueness: { scope: :invitee_id }
 end
