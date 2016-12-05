165e3a77dc689c45a5ca89bb1c25c75ae029fa07
Francisco Suero <suerofrancisco@yahoo.com>
2016-12-05 14:30:32 -0500

    done

--- app/models/tennant.rb
+++ app/models/tennant.rb
@@ -1,4 +1,4 @@
 class Tennant < ApplicationRecord
-  has_many :tennants_neighbors
-  has_many :neighbors, through: :tennants_neighbors, source: :neighbor
+  has_many :tennants_neighbors, foreign_key: :resident_id, dependent: :nullify
+  has_many :neighbors, through: :tennants_neighbors, source: :neighbor,:dependent => :nullify
 end
--- app/models/tennants_neighbor.rb
+++ app/models/tennants_neighbor.rb
@@ -1,4 +1,4 @@
 class TennantsNeighbor < ApplicationRecord
-  belongs_to :resident, foreign_key: :resident_id, class_name: "Tennant"
-  belongs_to :neighbor, foreign_key: :neighbor_id, class_name: "Tennant"
+  belongs_to :resident, foreign_key: :resident_id, class_name: "Tennant", dependent: :destroy
+  belongs_to :neighbor, foreign_key: :neighbor_id, class_name: "Tennant", :dependent => :destroy
 end
