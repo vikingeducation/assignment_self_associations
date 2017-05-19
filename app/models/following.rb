186d706f9f8d6f8bcf32c53bca02fe96184c60f4
Visiona <darbis@gmail.com>
2017-05-19 16:45:56 +0100

    Updated associations

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,12 +1,16 @@
 class Following < ApplicationRecord
 
-  belongs_to :acceptor_followings, 
+  #initiating following others side
+  belongs_to :follower_initiator, 
+              :class_name => "Person", 
+              :foreign_key => :sender_id
+
+  #accepting followers side
+  belongs_to :follower_acceptor, 
               :class_name => "Person", 
               :foreign_key => :acceptor_id
 
-  belongs_to :sender_followings, 
-              :class_name => "Person", 
-              :foreign_key => :sender_id
+
 
   validates :acceptor_id, :uniqueness => { :scope => :sender_id}
 
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,17 +1,19 @@
 class Person < ApplicationRecord
 
-  has_many :accepted_followings, 
-            :foreign_key => :follower_id,
-            :class_name => "Followings"
-  has_many :followers, 
-            :through => "Followings", 
-            :source => :sender_id
-
   has_many :send_followings, 
-            :foreign_key => :celeb_id, 
-            :class_name => "Followings"
-  has_many :celebs, 
-            :through => "Followings", 
-            :source => :acceptor_id
+            :class_name => "Following",
+            :foreign_key => :sender_id
+
+  has_many :followed_celebs, 
+            :through => :send_followings,
+            :source => :follower_acceptor
+
+  has_many :accepted_followings, 
+            :foreign_key => :acceptor_id,
+            :class_name => "Following"
+  has_many :users_followed_by, 
+            :through => :accepted_followings,
+            :source => :follower_initiator
+
 
 end
