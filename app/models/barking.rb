e2a594b9412937f6b0b2b57b758575940f00b213
Roy Chen <roychenlei@gmail.com>
2017-09-12 12:51:30 +0800

    setup associations

--- app/models/barking.rb
+++ app/models/barking.rb
@@ -1,2 +1,10 @@
 class Barking < ApplicationRecord
+  # Our barking Dog, i.e. the initiator
+  belongs_to :barker, foreign_key: :barker_id, class_name: "Dog"
+
+  # Our Dog being barked at, i.e. the recipient
+  belongs_to :barkee, foreign_key: :barkee_id, class_name: "Dog"
+
+  # ensures that our initiator/recipient pairs are unique
+  validates :barker_id, uniqueness: { scope: :barkee_id }
 end
