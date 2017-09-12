b096933516e86cec9f8e56bb139023fb1a9fbf57
Roy Chen <roychenlei@gmail.com>
2017-09-12 13:22:17 +0800

    setup dependent: :destroy for associations

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,9 @@
 class Dog < ApplicationRecord
   # when our Dog is a barker, i.e. he's barking at other Dogs (bad boy!)
-  has_many :barks_at, foreign_key: :barker_id, class_name: "Barking"
+  has_many :barks_at, foreign_key: :barker_id, class_name: "Barking", dependent: :destroy
   has_many :barkees, through: :barks_at, source: :barkee
 
   # when our Dog is a barkee, i.e. he's being barked at by other Dogs (poor boy!)
-  has_many :is_barked_at_by, foreign_key: :barkee_id, class_name: "Barking"
+  has_many :is_barked_at_by, foreign_key: :barkee_id, class_name: "Barking", dependent: :destroy
   has_many :barkers, through: :is_barked_at_by, source: :barker
 end
