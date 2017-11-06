cbcfc4e4d0850a55c98381eaba1b26eb64b83664
Sean Luckett <whitecrane007@gmail.com>
2017-11-06 16:42:41 -0700

    Deletable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :mentorings, foreign_key: :mentor_id, class_name: 'Mentorship'
+  has_many :mentorings, foreign_key: :mentor_id, class_name: 'Mentorship', dependent: :destroy
   has_many :disciples, through: :mentorings, source: :mentee
 
-  has_many :learnings, foreign_key: :mentee_id, class_name: 'Mentorship'
+  has_many :learnings, foreign_key: :mentee_id, class_name: 'Mentorship', dependent: :destroy
   has_many :teachers, through: :learnings, source: :mentor
 end
