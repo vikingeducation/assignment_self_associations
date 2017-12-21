b968b529c79272fd8073fd7041aa9fb764279ac7
anne <richardson.ae@gmail.com>
2017-12-21 16:22:36 -0600

    Sets up 1:X association for Employee model

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ assignment_self_associations
 ============================
 
 This is the self-reflective association. This is the self-reflective association. This is the self-reflective association. This is the self-reflective association...
+
+Anne Richardson
+
+## 1:X Association
+
+I set up an employees table that has a `manager_id` option.
+
+## X:X Association
+
+Users & Followers
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+shirley = Employee.create!(name: 'Shirley', role: 'manager', manager_id: nil)
+Employee.create!(name: 'Loki', role: 'emotional support', manager_id: shirley.id)
+Employee.create!(name: 'Thor', role: 'string duty', manager_id: shirley.id)
+nancylee = Employee.create!(name: 'Nancy Lee', role: 'string duty', manager_id: nil)
+Employee.create!(name: 'Alex', role: 'guard', manager_id: nancylee.id)
+
+
+papa = Employee.create!(name: 'Papa Smurf')
+Employee.create!(name: 'Hefty Smurf', manager_id: papa.id)
+Employee.create!(name: 'Vanity Smurf', manager_id: papa.id)
+Employee.create!(name: 'Smurfette', manager_id: papa.id)
+
+gmel = Employee.create!(name: 'Gargamel')
+Employee.create!(name: 'Azrael', manager_id: gmel.id)
+
+
+
+
+
